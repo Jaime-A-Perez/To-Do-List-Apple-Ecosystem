37966218d92cf50822f420e24d7f1ef5eca7fb48
Diana Paez <developermerci@gmail.com>
2023-12-05 15:44:08 -0500

    🎨 Add AuthModels folder with UserModel and KeychainManagerModel
    Create AuthModels directory to organize authentication related models.
    Include UserModel.swift for user data representation.
    Add KeychainManagerModel.swift for secure keychain operations.

---
+++ ToDoListAppPro/ToDoListAppPro/Models/AuthModels/KeychainManagerModel.swift
@@ -0,0 +1,8 @@
+//
+//  KeychainManagerModel.swift
+//  ToDoListAppPro
+//
+//  Created by Diana Paez on 5/12/23.
+//
+
+import Foundation
---
+++ ToDoListAppPro/ToDoListAppPro/Models/AuthModels/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  ToDoListAppPro
+//
+//  Created by Diana Paez on 5/12/23.
+//
+
+import Foundation
